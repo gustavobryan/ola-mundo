bed4fc9076186224a3263d2bc018f8449ffbd0ec
GustavoBryan <devgustavob@gmail.com>
2023-05-02 15:12:46 -0300

    posts e rodapé

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,12 +1,20 @@
-import Banner from "componentes/Banner";
+import styles from './Inicio.module.css';
+import Banner from 'componentes/Banner';
+import Post from 'componentes/Post';
+import posts from 'json/posts.json';
 
 export default function Inicio() {
     return (
         <main>
             <Banner />
 
-            <h1> Olá Mundo!</h1>
-
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
